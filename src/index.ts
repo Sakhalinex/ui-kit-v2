c150d994bedd0daa56dd66bebf7825cd131bd504
Dmitrii An <dmitrii.an@100hp.game>
2025-07-06 09:55:19 +0800

    update variables

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { BaseButton } from './common/BaseButton/BaseButton';
 export { Form } from './common/Form/Form';
 export { Input } from './common/Input/Input';
+export * from './theme.module.scss';
